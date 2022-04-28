4f8d766e633d9981469d574466224a65a542a8e3
guitarisprettierthanpiano <shfabrizio@gmail.com>
2022-04-27 23:17:25 -0400

    deploying maybe

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 8000;
-const path = require('path')
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   //server static content
   //npm run build
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, 'frontend/build')));
 }
 
 //routes
@@ -18,9 +18,11 @@ const authRoute = require('./routes/authRoute');
 const dashboardRouter = require('./routes/dashboard');
 app.use('/auth', authRoute);
 app.use('/dashboard', dashboardRouter);
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/build/index.html"));
-});
+app.get('*', (req, res) =>
+  res.sendFile(
+    path.resolve(__dirname, 'frontend', 'build', 'index.html')
+  )
+);
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
