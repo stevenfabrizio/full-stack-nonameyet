1dd8ab9201d01fbdb1c109ea8adeaadd10b182f4
guitarisprettierthanpiano <shfabrizio@gmail.com>
2022-05-03 05:03:51 -0400

    make it better

--- db.js
+++ db.js
@@ -4,22 +4,22 @@ const Pool = require('pg').Pool;
 //comment out PART 2 for local environment. uncomment PART 1
 
 //DEVELOPMENT MODE
-const pool = new Pool({
-  user: 'postgres',
-  password: 'idk',
-  host: 'localhost',
-  port: 5432,
-  database: 'fullstacker',
-});
+// const pool = new Pool({
+//   user: 'postgres',
+//   password: 'idk',
+//   host: 'localhost',
+//   port: 5432,
+//   database: 'fullstacker',
+// });
 
 //PRODUCTION MODE
-// const proConfig = process.env.DATABASE_URL; //heroku addons
+const proConfig = process.env.DATABASE_URL; //heroku addons
 
-// const pool = new Pool({
-//   connectionString: proConfig,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// });
+const pool = new Pool({
+  connectionString: proConfig,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 module.exports = pool;
